aa7d37553d0b3b7b16e7ae936c6126ff5a271c6c
unknown <adiflint27@gmail.com>
2019-11-30 13:55:49 +0200

    theProject

--- src/Term.java
+++ src/Term.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Term {
 //    private boolean mIsAlwaysCapital;
     private String m_Term;
-    private  TermDocFrequency[] m_DocFrequencies;
+    private ArrayList<TermDocFrequency> m_DocFrequencies;
     public Term(String term) {
         this.m_Term = term;
     }
 
-    public Term(String term, TermDocFrequency[] docFrequencies){
+    public Term(String term, ArrayList<TermDocFrequency> docFrequencies){
         this.m_Term = term;
         this.m_DocFrequencies = docFrequencies;
     }
 
-    public void updateTerm(Doc document){
-
+    public void updateTerm(String docId, int numOccurrences, int overallNumOfWords){
+        this.m_DocFrequencies.add(new TermDocFrequency(docId, numOccurrences, overallNumOfWords));
     }
 
     String serialize(){
-        String[] serializedDocFreqs = new String[this.m_DocFrequencies.length];
-        for (int i=0; i< this.m_DocFrequencies.length; i++){
-            serializedDocFreqs[i] = this.m_DocFrequencies[i].serialize();
+        String[] serializedDocFreqs = new String[this.m_DocFrequencies.size()];
+        for (int i=0; i< this.m_DocFrequencies.size(); i++){
+            serializedDocFreqs[i] = this.m_DocFrequencies.get(i).serialize();
         }
 
         return "" + this.m_Term + "::" + String.join(",", serializedDocFreqs);
@@ -30,9 +31,9 @@ public class Term {
         String[] splitted = serializedObj.split("[:][:]");
         String term = splitted[0];
         String[] splittedDocFreqs = splitted[1].split("[,]");
-        TermDocFrequency[] docFreqs =  new TermDocFrequency[splittedDocFreqs.length];
-        for (int i=0; i< splittedDocFreqs.length; i++){
-            docFreqs[i] =TermDocFrequency.deserialize(splittedDocFreqs[i]);
+        ArrayList<TermDocFrequency> docFreqs =  new ArrayList<TermDocFrequency>();
+        for (String splittedDocFreq : splittedDocFreqs) {
+            docFreqs.add(TermDocFrequency.deserialize(splittedDocFreq));
         }
         return new Term(term, docFreqs);
     }
@@ -42,45 +43,45 @@ public class Term {
      * add a doc to this term
      * @param document
      */
-    private void addDocToTerm(Doc document){
-        if(! m_DicOfDocs.containsKey(document)){
-            m_DicOfDocs.put(document.getDocNum(),1);
-        }
-        else {
-            int frequency = m_DicOfDocs.get(document.getDocNum());
-            m_DicOfDocs.replace(document.getDocNum(),frequency,++frequency);
-        }
-    }
+//    private void addDocToTerm(Doc document){
+//        if(! m_DicOfDocs.containsKey(document)){
+//            m_DicOfDocs.put(document.getDocNum(),1);
+//        }
+//        else {
+//            int frequency = m_DicOfDocs.get(document.getDocNum());
+//            m_DicOfDocs.replace(document.getDocNum(),frequency,++frequency);
+//        }
+//    }
 
-    private int df(){
-        return m_DicOfDocs.size();
-    }
-
-    private boolean containCapital(String str) {
-        return (Character.isUpperCase(str.charAt(0)));
-    }
-
-    private int tfInDocument(String docId){
-        return m_DicOfDocs.get(docId);
-    }
-
-    private int getTotalTf(){
-        int total = 0;
-        for(String doc : m_DicOfDocs.keySet()){
-            total = total + m_DicOfDocs.get(doc);
-        }
-        return total;
-    }
-
-    public boolean isM_isAlwaysCapital() {
-        return m_isAlwaysCapital;
-    }
-
-    public String getM_term() {
-        return m_term;
-    }
-
-    public HashMap<String, Integer> getM_DicOfDocs() {
-        return m_DicOfDocs;
-    }
+//    private int df(){
+//        return m_DicOfDocs.size();
+//    }
+//
+//    private boolean containCapital(String str) {
+//        return (Character.isUpperCase(str.charAt(0)));
+//    }
+//
+//    private int tfInDocument(String docId){
+//        return m_DicOfDocs.get(docId);
+//    }
+//
+//    private int getTotalTf(){
+//        int total = 0;
+//        for(String doc : m_DicOfDocs.keySet()){
+//            total = total + m_DicOfDocs.get(doc);
+//        }
+//        return total;
+//    }
+//
+//    public boolean isM_isAlwaysCapital() {
+//        return m_isAlwaysCapital;
+//    }
+//
+//    public String getM_term() {
+//        return m_term;
+//    }
+//
+//    public HashMap<String, Integer> getM_DicOfDocs() {
+//        return m_DicOfDocs;
+//    }
 }
